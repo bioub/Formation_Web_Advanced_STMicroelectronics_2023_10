10fc644d32e5ff127faf0eaf77d6ef1b1532ec27
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-10-09 16:04:09 +0200

    Chalk

--- Modules/demo-commonjs-node/main.js
+++ Modules/demo-commonjs-node/main.js
@@ -1,9 +1,9 @@
 const { sum, sub } = require('./my-math'); // si le fichier est local au projet toujours préfixer par ./ (le dossier courant) ou ../ le dossier parent
 const hello = require('./hello'); // si le fichier est local au projet toujours préfixer par ./ (le dossier courant) ou ../ le dossier parent
-
+const chalk = require('chalk');
 
 console.log(sum(1, 2)); // 3
 console.log(sub(1, 2)); // -1
-console.log(hello('Romain')); // Hello Romain
+console.log(chalk.red(hello('Romain'))); // Hello Romain
 
 
