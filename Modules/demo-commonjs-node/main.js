c3cff23d69705cb1200ce4606cc1389a37c5c554
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-10-10 10:38:32 +0200

    Rappels Jour 1

--- Modules/demo-commonjs-node/main.js
+++ Modules/demo-commonjs-node/main.js
@@ -1,7 +1,7 @@
 const { sum, sub } = require('./my-math'); // si le fichier est local au projet toujours préfixer par ./ (le dossier courant) ou ../ le dossier parent
 const hello = require('./hello'); // si le fichier est local au projet toujours préfixer par ./ (le dossier courant) ou ../ le dossier parent
-const chalk = require('chalk');
+const { red } = require('chalk');
 
 console.log(sum(1, 2)); // 3
 console.log(sub(1, 2)); // -1
-console.log(chalk.red(hello('Romain'))); // Hello Romain
+console.log(red(hello('Romain'))); // Hello Romain
