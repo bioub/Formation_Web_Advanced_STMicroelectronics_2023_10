91f89e93593fbb0e81ac01ca1a10716c096a88d6
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-10-20 16:27:28 +0200

    Debut React

---
+++ react/src/app/app.tsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+
+export function App() {
+  return (
+    <div>
+      Hello
+    </div>
+  );
+}
+
+export default App;
